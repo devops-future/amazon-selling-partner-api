3390364da3260c8660716bd07c8e71a4f918cc5e
devops-future <gice101582@gmail.com>
2021-10-04 17:32:54 +0800

    portal, admin, theme, test package added

--- packages/DevOpsFuture/TestPackage/src/Http/Controllers/Portal/TestPackageController.php
+++ packages/DevOpsFuture/TestPackage/src/Http/Controllers/Portal/TestPackageController.php
@@ -5,6 +5,7 @@ namespace DevOpsFuture\TestPackage\Http\Controllers\Portal;
 use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use SellingPartnerApi;
 
 class TestPackageController extends Controller
 {
@@ -34,7 +35,18 @@ class TestPackageController extends Controller
      */
     public function index()
     {
-        print_r('test');
+        print_r(env('LWA_CLIENT_SECRET'));
+        $config = new SellingPartnerApi\Configuration(
+            [
+                "lwaClientId" => env('LWA_CLIENT_SECRET'),
+                "lwaClientSecret" => env('LWA_CLIENT_ID'),
+                "lwaRefreshToken" => env('LWA_REFRESH_TOKEN'),
+                "awsAccessKeyId" => env('AWS_ACCESS_KEY_ID'),
+                "awsSecretAccessKey" => env('AWS_SECRET_ACCESS_KEY'),
+                "endpoint" => env('AWS_ENDPOINT'),
+                'roleArn' => env('AWS_ROLE_ARN')
+            ]
+        );
         return view($this->_config['view']);
     }
 }
